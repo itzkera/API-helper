f5913255812b6a396c376300bb332f60d36a0cc8
Nick <95004283+itzkera@users.noreply.github.com>
2025-10-02 18:32:56 -0400

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { scanEndpoint} from "./scanendpoints;
+
+
+const baseUrl = process.argv[2];
+const endpoint = process.argv.slice(3);
+
+
+if (!baseUrl || endpoint.length === 0) {
+  console.log("Usage: bun run src/index.ts <baseUrl> <endpoint1> <endpoint2> CLOSING!!");
+  process.exit(1);
+}
+
+async function main() {
+  console.log(`Scanning ${baseUrl] for endpoints:`, endpoints);
+  console.log("welcome to Kera's API finder pls star");
+const results = await scanEndpoint(baseUrl, endpoints);
+ for (const result of results) {
+    console.log(`[${result.status}] ${result.url}`);
+  }
+}
+
+
+main();
